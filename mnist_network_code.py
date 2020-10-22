bc643db4afee4fd8092873600165aa06a857f939
Intern097 <john.theocharides@gmail.com>
2020-10-22 17:19:24 +0300

    Added Gradient Tape to model(now working)

--- mnist_network_code.py
+++ mnist_network_code.py
@@ -10,7 +10,7 @@ from keras.layers import Flatten, Dense, Dropout
 from keras.models import Sequential
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.optimizers import Adam
-
+import math
 
 class Model():
 
@@ -20,6 +20,8 @@ class Model():
         self.x_train, self.x_test = self.x_train/255.0, self.x_test/255.0
         self.y_train = k.cast(self.y_train, 'float32')
         self.y_test = k.cast(self.y_test, 'float32')
+        self.batch_size = 128
+        self.epochs = 20
         # Model
         self.model = self.build_model()
         self.opt = Adam()
@@ -41,7 +43,7 @@ class Model():
         model.add(outer_layer)
         return model
 
-    def gradient_tape(self, x_true, y_true):
+    def step(self, x_true, y_true):
         with tf.GradientTape() as tape:
             pred = self.model(x_true)
             loss = sparse_categorical_crossentropy(y_true, pred)
@@ -49,23 +51,28 @@ class Model():
         grads = tape.gradient(loss, self.model.trainable_variables)
         self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
 
-
     def create_loss(self, y_true, y_pred):
         loss = k.sum(k.log(y_true) - k.log(y_pred))
         return loss
 
     def compile_model(self):
+        bat_per_epoch = math.floor(len(self.x_train) / self.batch_size)
+        for epoch in range(self.epochs):
+            print('=', end='')
+            for i in range(bat_per_epoch):
+                n = i * self.batch_size
+                self.step(self.x_train[n:n + self.batch_size], self.y_train[n:n + self.batch_size])
         self.model.compile(loss='sparse_categorical_crossentropy', optimizer=self.opt, metrics=['accuracy'])
 
     def fit_model(self):
-        self.model.fit(self.x_train, self.y_train, epochs=5)
+        self.model.fit(self.x_train, self.y_train, epochs=self.epochs)
 
     def return_score(self):
         score = self.model.evaluate(self.x_test, self.y_test)
         print('accuracy', score[1])
 
 
-class Loss_function():
+class Loss_function:
 
     @staticmethod
     def loss(y_true, y_pred):
@@ -73,21 +80,6 @@ class Loss_function():
         return loss
 
 
-# class optimizer:
-#
-#     def __init__(self):
-#         self.model = Model()
-#
-#     def loss(self, y_true, y_pred):
-#         with tf.GradientTape() as tape:
-#             loss = sparse_categorical_crossentropy(y_true, y_pred)
-#
-#         opt = Adam
-#         grads = tape.gradient(loss, self.model.trainable_variables)
-#         opt.apply_grdients(zip(grads, self.model.trainable_variables))
-#         return opt
-        
-        
 mnist = keras.datasets.mnist
 x = Model(mnist.load_data())
 x.return_score()
